72c893299f2dc5b36f6f3f3c4236753316005bc2
KeruFC <kerubela1@gmail.com>
2020-10-04 19:24:09 +0200

    add data classes

--- app/src/main/java/com/example/madlevel3task2/PortalsFragment.kt
+++ app/src/main/java/com/example/madlevel3task2/PortalsFragment.kt
@@ -7,11 +7,16 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import com.example.madlevel3task2.databinding.ActivityMainBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class PortalsFragment : Fragment() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val portals = arrayListOf<Portal>()
+    private val portalAdapter = PortalAdapter(portals)
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
